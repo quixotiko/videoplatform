0c4564c46a20a3d1b4f367c156eda88c1fddecdb
quixotiko <1686372648@qq.com>
2019-08-15 23:38:52 +0800

    :tada:

--- src/api/api.js
+++ src/api/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
-const BASE_URL = '';
-const HOME_PAGE_URL = `/api/v4/tabs/selected`;
-const VIDEO_CATEGORY_URL = `/api/v4/discovery/category`;
-const VIDEO_HOT = `/api/v4/discovery/hot`;
-const VIDEO_DETAIL_URL = `/api/v1/video/`;
-const RELATE_VIDEO_LIST_URL = `/api/v1/video/related/`;
-const REPLIES_VIDEO_LIST_URL = `/api/v1/replies/video?id=`
-const SEARCH_URL = `/api/v1/search?num=15&query=`
+const BASE_URL = 'https://bird.ioliu.cn/v1?url=';
+const HOME_PAGE_URL = `${BASE_URL}http://baobab.kaiyanapp.com/api/v4/tabs/selected`;
+const VIDEO_CATEGORY_URL = `${BASE_URL}http://baobab.kaiyanapp.com/api/v4/discovery/category`;
+const VIDEO_HOT = `${BASE_URL}http://baobab.kaiyanapp.com/api/v4/discovery/hot`;
+const VIDEO_DETAIL_URL = `${BASE_URL}http://baobab.wandoujia.com/api/v1/video/`;
+const RELATE_VIDEO_LIST_URL = `${BASE_URL}http://baobab.wandoujia.com/api/v1/video/related/`;
+const REPLIES_VIDEO_LIST_URL = `${BASE_URL}http://baobab.wandoujia.com/api/v1/replies/video?id=`
+const SEARCH_URL = `${BASE_URL}http://baobab.kaiyanapp.com/api/v1/search?num=15&query=`
 
 
 export {
